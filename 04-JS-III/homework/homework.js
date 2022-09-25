1053fc545633da2cb31d84b52ad6f745af07daf7
CristianSombra <cristiansombra87@gmail.com>
2022-09-25 10:21:47 -0300

    Exercises 25/9/2022

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -132,8 +132,16 @@ function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+    var cajita = 0;
 
-        return Math.max(...numeros)
+    for (var i = 0; i < numeros.length; i++)
+        if (numeros[i] > cajita) {
+            cajita = numeros[i];
+
+        }
+
+    return cajita;	
+
 
 }
 
@@ -142,31 +150,69 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+    var acumulador = 1;
 
+    if (arguments.length === 0) {
+        return 0;
+
+    } else if (arguments.lenght === 1) {
+        return arguments[0];
+
+    } else {
+        for (var i = 0; i < arguments.length; i++) {
+            acumulador = acumulador * arguments[i]
+
+        }
+
+    }
+
+    return acumulador;
 }
 
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
+    var contador = 0;
+
+    for (var i = 0; i < arreglo.length; i++) {
+        if (arreglo[i] > 18) {
+            contador += 1
+        }
+
+    }
+
+    return contador;
+
 
 }
 
 
 function diaDeLaSemana(numeroDeDia) {
-  //Suponga que los días de la semana se codifican como 1 = Domingo, 2 = Lunes y así sucesivamente. 
+  //Suponga que los días de la semana se codifican como 1 = Domingo, 2 = Lunes y así sucesivamente.
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
-  //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
-  //Escribe tu código aquí   
-  
+  //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario.
+  //Escribe tu código aquí
+
+    if (numeroDeDia === 1 || numeroDeDia === 7) {
+        return "Es fin de semana"
+
+    }
+    else if (numeroDeDia > 1 && numeroDeDia < 7) {
+        return "Es dia Laboral"
+
+    }
 } 
 
 
 function empiezaConNueve(n) {
-  //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
+  //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  
+
+
+
+
 }
 
 
