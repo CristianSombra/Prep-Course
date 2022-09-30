aa1cb25c723bc6f23348bb8dc7cd3b100b816649
CristianSombra <cristiansombra87@gmail.com>
2022-09-29 23:52:18 -0300

    Exercises 29/9/2022

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -265,27 +265,68 @@ function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+    var nuevoArray = []
+
+    for (var i = 0 ; i < array.length; i++) {
+        if (array[i] > 100) {
+            nuevoArray.push(array[i])
+        }
+
+    }
+
+    return nuevoArray;
+
 }
 
 
 function breakStatement(numero) {
   //Iterar en un bucle aumentando en 2 el numero recibido hasta un límite de 10 veces.
-  //Guardar cada nuevo valor en un array. 
+  //Guardar cada nuevo valor en un array.
   //Devolver el array
-  //Si en algún momento el valor de la suma y la cantidad de iteraciones coinciden, debe interrumpirse la ejecución y 
+  //Si en algún momento el valor de la suma y la cantidad de iteraciones coinciden, debe interrumpirse la ejecución y
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+    var arrayNumeros = [];
+    var suma = numero;
+
+    for (var i = 0; i < 10; i++) {
+        suma = suma + 2;
+        if (suma === i) {
+            break;
+        } else {
+            arrayNumeros.push(suma);
+        }
+    }
+    if (arrayNumeros.length < 10) {
+        return "Se interrumpió la ejecución";
+    } else if (arrayNumeros.length === 10) {
+        return arrayNumeros;
+    }
+
 }
 
 
 function continueStatement(numero) {
   //Iterar en un bucle aumentando en 2 el numero recibido hasta un límite de 10 veces.
-  //Guardar cada nuevo valor en un array.    
+  //Guardar cada nuevo valor en un array.
   //Devolver el array
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+    var arrayNumeros = [];
+
+    var suma = numero;
+
+    for (var i = 0; i < 10; i++) {
+        if (i === 5) {
+            continue;
+        } else {
+            suma = suma + 2;
+            arrayNumeros.push(suma)
+        }
+    }
+    return arrayNumeros;
 }
 
 
